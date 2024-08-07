b6b4b21d49ef1cf5811c67b1be69cbdecad2ae8f
hwookim <gusdn9754@gmail.com>
2024-08-07 20:59:25 +0900

    mui Select

---
+++ hw/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import BasicSelect from "./Component";
+
+function App() {
+  return (
+    <>
+      <BasicSelect />
+    </>
+  );
+}
+
+export default App;
---
+++ hw/src/Component.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import styles from "./Component.module.scss";
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ marginTop: 100, minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+          MenuProps={{
+            classes: {
+              paper: styles.my,
+            },
+            slotProps: {
+              paper: {
+                className: "가능은함",
+              },
+              root: {},
+            },
+          }}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
