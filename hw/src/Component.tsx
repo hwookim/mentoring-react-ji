e375ab39b477957dcc02cf36457e9837e316b9ce
hwookim <gusdn9754@gmail.com>
2024-08-07 21:31:52 +0900

    mouse enter - leave

--- hw/src/Component.tsx
+++ hw/src/Component.tsx
@@ -5,41 +5,67 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import styles from "./Component.module.scss";
+import { Button } from "@mui/material";
 
 export default function BasicSelect() {
-  const [age, setAge] = React.useState("");
+  const [age, setAge] = React.useState("10");
+  const [mouseIn, setMouseIn] = React.useState(false);
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value as string);
   };
 
   return (
-    <Box sx={{ marginTop: 100, minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-          MenuProps={{
-            classes: {
-              paper: styles.my,
-            },
-            slotProps: {
-              paper: {
-                className: "가능은함",
+    <>
+      <Button
+        variant="outlined"
+        onMouseEnter={() => setMouseIn(true)}
+        onMouseLeave={() => setMouseIn(false)}
+      >
+        {mouseIn ? <div>"yyyy"</div> : <img alt="????" />}
+      </Button>
+      <Box sx={{ marginTop: 100, minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+            MenuProps={{
+              classes: {
+                paper: styles.my,
               },
-              root: {},
-            },
-          }}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+              slotProps: {
+                paper: {
+                  className: "가능은함",
+                },
+                root: {},
+              },
+            }}
+          >
+            {[
+              { value: 10, text: "Ten" },
+              { value: 20, text: "Twenty" },
+              { value: 30, text: "Thirty" },
+            ].map((item) => (
+              <MenuItem
+                key={item.value}
+                value={item.value}
+                className={styles.menuItem}
+                sx={{
+                  "&.Mui-selected": {
+                    backgroundColor: "red",
+                  },
+                }}
+              >
+                {item.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </>
   );
 }
